6ac6e3ebb9d211560edc0a9e00829f373ea05847
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-02-25 20:57:48 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './App.css';
->>>>>>> 80e5ff939f4c6af957f203be5293a1f4bee98d5f
+
 class App extends Component {
   handleOnClick() {
     this.props.dispatch({
