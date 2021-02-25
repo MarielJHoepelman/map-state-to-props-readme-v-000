f8c4240e33eda0a1ba3711bac8364e5a4717cd89
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-02-25 20:55:44 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom";
 import { createStore } from "redux";
@@ -5,15 +6,32 @@ import { Provider } from "react-redux";
 import shoppingListItemReducer from "./reducers/shoppingListItemReducer";
 import App from "./App";
 import "./index.css";
+=======
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import shoppingListItemReducer from './reducers/shoppingListItemReducer';
+import App from './App';
+import './index.css';
+>>>>>>> 80e5ff939f4c6af957f203be5293a1f4bee98d5f
+
 
 const store = createStore(
-  shoppingListItemReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+ shoppingListItemReducer,
+ window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 ReactDOM.render(
+<<<<<<< HEAD
   <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById("root")
+=======
+ <Provider store={store}>
+   <App />
+ </Provider>, /* code change */
+ document.getElementById('root')
+>>>>>>> 80e5ff939f4c6af957f203be5293a1f4bee98d5f
 );
